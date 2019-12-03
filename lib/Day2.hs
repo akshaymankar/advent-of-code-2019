6a6d5e8df7d3e54ce39b8b4503bbb841d92eb157
Akshay Mankar <itsakshaymankar@gmail.com>
2019-12-03 03:27:54 +0000

    Use Seq instead if Array in Day2
    Arrays either copy themselves all over the memory or are painful to use. Seqs on
    the other hand perform much better for simple looking code

--- lib/Day2.hs
+++ lib/Day2.hs
@@ -1,8 +1,8 @@
 module Day2 where
 
-import Text.ParserCombinators.ReadP
 import Data.Char
-import Data.Array
+import Data.Sequence
+import Text.ParserCombinators.ReadP
 
 day2_1 :: IO ()
 day2_1 = do
@@ -14,32 +14,36 @@ day2_2 = do
   intcode <- readInput
   let result = head [100 * noun + verb | noun <- [0..99]
                                        , verb <- [0..99]
-                                       , let res = executeIntcode (withNounAndVerb noun verb intcode)
-                                         in res == 19690720]
+                                       , executeIntcode (withNounAndVerb noun verb intcode) == 19690720
+                                       ]
   print result
 
-type Code = Array Int Int
+type Code = Seq Int
 type Pos = Int
 type Noun = Int
 type Verb = Int
 
 withNounAndVerb :: Noun -> Verb -> Code -> Code
-withNounAndVerb noun verb code = code // [(1,noun), (2,verb)]
+withNounAndVerb noun verb code =
+  update 1 noun $ update 2 verb code
 
 executeIntcode :: Code -> Int
 executeIntcode input =
-  snd (go (0, input)) ! 0
+  snd (go (0, input)) `index` 0
   where
     go :: (Pos, Code) -> (Pos, Code)
     go (pos, code) =
-      let op = code ! pos
+      let op = code `index` pos
       in case op of
         1 -> go (pos + 4, add (binaryOperands pos code) code)
         2 -> go (pos + 4, multiply (binaryOperands pos code) code)
         99 -> (pos, code)
         invalid -> error $ "Invalid operation: " ++ show invalid
     binaryOperands :: Pos -> Code -> (Int, Int, Int)
-    binaryOperands pos code = (code ! (pos + 1), code ! (pos + 2), code ! (pos + 3))
+    binaryOperands pos code = ( code `index` (pos + 1)
+                              , code `index` (pos + 2)
+                              , code `index` (pos + 3)
+                              )
 
 add :: (Int, Int, Int) -> Code -> Code
 add = binaryOperation (+)
@@ -49,15 +53,15 @@ multiply = binaryOperation (*)
 
 binaryOperation :: (Int -> Int -> Int) -> (Int, Int, Int) -> Code -> Code
 binaryOperation f (op1, op2, result) code =
-   let a = code ! op1
-       b = code ! op2
-  in code // [(result, a `f` b)]
+   let a = code `index` op1
+       b = code `index` op2
+  in update result (a `f` b) code
 
 readInput :: IO Code
 readInput = do
   inputStr <- getLine
   case readP_to_S inputReadP inputStr of
-    [(ints, "")] -> pure $ listArray (0, length ints - 1) ints
+    [(ints, "")] -> pure $ fromList ints
     _ -> error $ "Failed to parse '" ++ inputStr ++ "'"
 
 inputReadP :: ReadP [Int]
